5fb74fc41bf832cbf4660fc8ea34f2cd5d30ea31
Eypan <eypan@ualberta.ca>
2026-03-02 13:18:12 -0700

    Added testDeleteCity, failing build

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,13 +4,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomListTest {
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
+
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+
+        // Verify the city is in the lists
+        assertTrue(list.hasCity(edmonton));
+
+        // Delete the city
+        list.deleteCity(edmonton);
+
+        // Verify if it was removed
+        assertFalse(list.hasCity(edmonton));
     }
 
 }
